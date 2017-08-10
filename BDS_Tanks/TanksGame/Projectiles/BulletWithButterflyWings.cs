c6f1157e0398145c0c57c3646e0c65a2b8ae3364
ekov1 <kristian_ekov@abv.bg>
2017-08-10 11:53:55 +0300

    pjectiles++

--- BDS_Tanks/TanksGame/Projectiles/BulletWithButterflyWings.cs
+++ BDS_Tanks/TanksGame/Projectiles/BulletWithButterflyWings.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TanksGame.Core.Contracts;
+using TanksGame.Core.Providers;
+using TanksGame.Environment;
 
 namespace TanksGame.Projectiles
 {
     class BulletWithButterflyWings : Projectile
     {
-        public Rocket(int x, int y, int damage) : base(x, y, damage)
+        IBoolTemplateProvider templateProvider;
+
+        public BulletWithButterflyWings(int x, int y, int damage) : base(x, y, damage)
         {
             this.templateProvider = BoolTemplateProvider.Instace;
 
+        }
+
+        public override Texture Texture
+        {
+            get
+            {
+                return new Texture(this.templateProvider.GetBoolTemplate("BulletWithButterflyWings"), 'x', ConsoleColor.Red);
+            }
         }
     }
 }
